29c47b996e5269850cae607c06d74e6a75f52909
turtle-insect <turtle.insect@gmail.com>
2017-10-28 01:55:04 +0900

    Add Princess's Param

--- DQ1/DataContext.cs
+++ DQ1/DataContext.cs
@@ -22,5 +22,18 @@ namespace DQ1
 				Util.WriteNumber(0x5C, 4, value, 0, 99999999);
 			}
 		}
+
+		public bool Princess
+		{
+			get
+			{
+				return SaveData.Instance().ReadNumber(0xC4, 1) == 1;
+			}
+
+			set
+			{
+				SaveData.Instance().WriteNumber(0xC4, 1, value ? 1U : 0);
+			}
+		}
 	}
 }
